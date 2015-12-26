02447c4f3dc427ebbdf9534fcefbc52c7b12d12d
A. Bram Neijt <bneijt@gmail.com>
2015-12-27 00:00:29 +0100

    Out of memory again

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ function initDb() {
             r.db("zinnug").tableList().run(connection, function(error, result) { return done(error, result, connection);});
         },
         function checkTables(existingTables, connection, done) {
-            var tables = ['words', 'wordTransitions'],
+            var tables = ['words', 'triplets', 'wordTransitions'],
                 lastTable = 'wordTransitions';
-
+            //TODO race condition here, we need to async.series the separate tables
             _.forEach(tables, function(tableName){
                 if(existingTables.indexOf(tableName) >= 0) {
                     console.log("Table " + tableName + " already exists");
@@ -68,6 +68,8 @@ function translate(haystack, searchReplaces) {
     return haystack;
 }
 
+
+
 function loadHetBoek() {
     async.waterfall([
         function openConnection(done) {
@@ -76,20 +78,55 @@ function loadHetBoek() {
         },
         function insertWords(connection, done) {
             //Split data files into words and put them in the databse
-            fs.readFile('data/Het-Boek.txt', {"encoding": "utf-8"}, function(error, contents) {
+            fs.readFile('data/Het-Boek.txt', {"encoding": "utf-8"}, function(error, wholeFile) {
                 if (error) {
                     throw err
                 }
                 console.log("Translate")
-                contents = translate(contents, [
+                contents = translate(wholeFile, [
                     /[^a-zA-Z]+/g, ' ',
                     / +/g, ' ',
                     ]);
+
+                //Each of the words
                 words = contents.split(" ");
+                words.sort();
+                words = _.uniq(words, true);
                 console.log("Loading words: " + words.length);
                 wordObjects = _.map(words, function(word) { return {id: word, lastInsertSource: "Het-Boek"};});
                 async.each(_.chunk(wordObjects, 500), function insertWordObjects(wordObjects, next) {
                     r.table("words").insert(wordObjects, {conflict: "replace"}).run(connection, next);
+                }, function(){ console.log("done"); done(null, connection)});
+            });
+        },
+        function insertTriplets(connection, done) {
+            //Split data files into words and put them in the databse
+            fs.readFile('data/Het-Boek.txt', {"encoding": "utf-8"}, function(error, wholeFile) {
+                if (error) {
+                    throw err
+                }
+                console.log("Loading triplets");
+
+                //Word triplets
+                words = translate(wholeFile, [
+                    /[^a-zA-Z.]+/g, ' ',
+                    /\./g, ' . ',
+                    / +/g, ' ',
+                    ]).split(" ");
+                triplets = _.map(words, function crTriplets(value, index, collection){
+                    var triplet = collection.slice(index, index + 3);
+                    if(triplet.length == 3) {
+                        return {id: triplet.join("-"), fst: triplet[0], snd: triplet[1], trd: triplet[2]};
+                    }
+                    return null;
+                });
+
+                triplets = _.reject(triplets, _.isNull);
+                console.log("Found " + triplets.length + " triplets");
+
+                //Insert triplets
+                async.each(_.chunk(triplets, 500), function insert(obj, next) {
+                    r.table("triplets").insert(obj, {conflict: "replace"}).run(connection, next);
                 }, function(){ console.log("done"); done(null, connection)});
             });
         },
@@ -100,6 +137,32 @@ function loadHetBoek() {
     ]);
 
 }
+function numberLetters(phoneNumber) {
+    return _.map(phoneNumber, function lettersForNumber(n){
+        return {
+            "0": "0",
+            "1": "1abc",
+            "2": "2def",
+            "3": "3ghi",
+            "4": "4jkl",
+            "5": "5mno",
+            "6": "6pqrs",
+            "7": "7tuv",
+            "8": "8wxyz",
+            "9": "9"
+        }[n];
+    });
+}
+function algo1(phoneNumber) {
+    //Look up all word combinations for the numbers
+    var lettersForEachNumber = numberLetters(phoneNumber);
+    console.log(lettersForEachNumber);
+    //Generate sentence by selecting matching firstletter words possible
+    //Calculate their score by checking the number of triplet matches
+    //Pick the highest scoring one
+
+    return phoneNumber;
+}
 
 function main(arguments) {
     //We have to do something to stop hitting a bug in drain()
@@ -108,11 +171,17 @@ function main(arguments) {
         console.log("Requires a command as first argument")
     }
     switch(command) {
-        case "initDb":
+        case "init":
+            //Run once after creating the database
             initDb();
             break;
-        case "loadData":
-            loadHetBoek()
+        case "load":
+            //Run as many times as the data changes
+            loadHetBoek();
+            break;
+        case "test":
+            var phoneNumber = "55776584"; //randomly selected phonenumber
+            algo1(phoneNumber);
             break;
         default:
             console.log("Unknown command " + command);
